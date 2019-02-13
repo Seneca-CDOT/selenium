379b35a17f59ebe75cf44326fe0251e51bca0888
miguel <mroncancio19@gmail.com>
2019-02-13 15:50:14 -0500

    implement constructors

--- java/client/src/org/openqa/selenium/pluma/PlumaDriver.java
+++ java/client/src/org/openqa/selenium/pluma/PlumaDriver.java
@@ -1,8 +1,8 @@
 package org.openqa.selenium.pluma;
 
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.service.DriverCommandExecutor;
 
 /**
  * a {@link WebDriver} implementation that controls a JSDOM object running on a local machine
@@ -11,6 +11,15 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class PlumaDriver extends RemoteWebDriver {
 
-  public PlumaDriver() { this (PlumaDriverService.createDefaultService(), new PlumaOptions()); }
+  public PlumaDriver() { this(PlumaDriverService.createDefaultService(), new PlumaOptions()); }
 
+  public PlumaDriver(PlumaDriverService service) { this(service, new PlumaOptions()); }
+
+  public PlumaDriver(PlumaDriverService service, PlumaOptions options) {
+    super(new DriverCommandExecutor(service), options);
+  }
+
+  public PlumaDriver(PlumaOptions options) {
+    this(PlumaDriverService.createDefaultService(), options);
+  }
 }
