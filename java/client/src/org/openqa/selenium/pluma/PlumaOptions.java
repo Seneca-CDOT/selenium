6495ed9474c3addc70efff8e7cc57c6dd0be3822
miguel <mroncancio19@gmail.com>
2019-02-13 15:49:17 -0500

    basic implementation of PlumaOptions, currently incomplete

--- java/client/src/org/openqa/selenium/pluma/PlumaOptions.java
+++ java/client/src/org/openqa/selenium/pluma/PlumaOptions.java
@@ -3,6 +3,7 @@ package org.openqa.selenium.pluma;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import org .openqa.selenium.Capabilities;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
@@ -22,13 +23,23 @@ public class PlumaOptions extends MutableCapabilities {
 
   public PlumaOptions () { setCapability(CapabilityType.BROWSER_NAME, BrowserType.PLUMA);}
 
+  @Override
+  public PlumaOptions merge(Capabilities extraCapabilities) {
+    super.merge(extraCapabilities);
+    return this;
+  }
+
+  // TODO: include method to allow script execution for browser
+  // TODO: include method to allow browser to intervene before parsing
+  // TODO: include method to load resources (iframes, stylesheets, scripts, images... need to integrate npm canvas in server source
+
+
   /**
    * Sets the path to the Pluma executable. The path should exist on the machine
    * which will launch Pluma. Path should either be absolute or relative
    * to the location of running PlumaDriver server.
    * @param path Path to Pluma executable
    */
-
   public PlumaOptions setBinary(File path) {
     binary = checkNotNull(path).getPath();
     return this;
@@ -40,12 +51,15 @@ public class PlumaOptions extends MutableCapabilities {
    * to the location of running PlumaDriver server.
    * @param path Path to Pluma executable
    */
-
   public PlumaOptions setBinary(String path) {
     binary = checkNotNull(path);
     return this;
   }
 
+  public PlumaOptions setProxy(Proxy proxy) {
+    setCapability(CapabilityType.PROXY, proxy);
+    return this;
+  }
 
 
 }
